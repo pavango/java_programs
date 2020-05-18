22749b85db063eab40f1e12dfb51d8a8a0497b82
pavangowda <pavangowda9623@gmail.com>
2020-05-18 18:11:25 +0530

    AreaOfRectangle

---
+++ basic_programs/AreaOfRectangle.java
@@ -0,0 +1,14 @@
+/*
+  Java program to calculate the area of rectangle.
+  Area of Rectangle = Hight * Width;
+  For this example, consider 10 as height and 5 as width.
+*/
+class AreaOfRectangle{
+  public static void main(String args[]) {
+    int Area, h, w;
+    h=10;
+    w=5;
+    Area = h * w;
+    System.out.println("AreaOfRectangleis: " + Area);
+  }
+}
