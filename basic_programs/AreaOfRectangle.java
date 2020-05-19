2db04051d7c5d01b08299d30ba7fd6a32207a819
pavangowda <pavangowda9623@gmail.com>
2020-05-19 10:26:53 +0530

    added user input

--- basic_programs/AreaOfRectangle.java
+++ basic_programs/AreaOfRectangle.java
@@ -1,15 +1,18 @@
+import java.util.*;
+
 /*
   Java program to calculate the area of rectangle.
   Area of Rectangle = Hight * Width;
   For this example, consider 10 as height and 5 as width.
 */
-class AreaOfRectangle{
-  public static void main(String args[]) {
-    int area, hight, width;
-    hight = 10;
-    width = 5;
-    area = hight * width;
-
-    System.out.println("Area Of Rectangle is: " + area);
-  }
+class AreaOfRectangle {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);    //System.in is a standard input stream
+        System.out.print("Enter the value of Hight:");
+        int height = sc.nextInt();
+        System.out.print("Enter the value of Width:");
+        int width = sc.nextInt();
+        int area = height * width;
+        System.out.println("Area Of Rectangle= " + area);
+    }
 }
