5322c70f2096d78e519885dae71b6015da508657
pavangowda <pavangowda9623@gmail.com>
2020-05-19 12:09:14 +0530

    AreaOFParallelogram.java

---
+++ basic_programs/AreaOFParallelogram.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class AreaOFParallelogram {
+    public static void main(StrinG args[]) {
+        Scanner sc = new Scanner(System.in);
+        int base,hight,area ;
+        System.out.print("Enter The Value Of Base:");
+        base = sc.nextInt();
+        System.out.print("Enter The Value Of Hight:");
+        hight = sc.nextInt();
+        area = base * hight;
+        System.out.println("The Area Of Parallelogram Is:" + area);
+    }
+}
