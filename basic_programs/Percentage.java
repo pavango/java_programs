16824bb49732d3537e2bbf5d8b63ac16aa36d65d
pavangowda <pavangowda9623@gmail.com>
2020-05-21 20:09:56 +0530

    modified program of %

--- basic_programs/Percentage.java
+++ basic_programs/Percentage.java
@@ -1,25 +1,20 @@
-import java.util.*;
+import java.util.Scanner;
 
-class Percentage {
-    public static void main(String args[]) {
+class Marks {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter The Student Name:");
-        String name = sc.nextLine();
-        System.out.print("Enter Subject 1 Marks:");
-        double subject1 = sc.nextDouble();
-        System.out.print("Enter Subject 2 Marks:");
-        double subject2 = sc.nextDouble();
-        System.out.print("Enter Subject 3 Marks:");
-        double subject3 = sc.nextDouble();
-        System.out.print("Enter Subject 4 Marks:");
-        double subject4 = sc.nextDouble();
-        System.out.print("Enter Subject 5 Marks:");
-        double subject5 = sc.nextDouble();
-        System.out.print("Enter Subject 6 Marks:");
-        double subject6 = sc.nextDouble();
-        double obtaind = subject1 + subject2 + subject3 + subject4 + subject5 + subject6;
-        double Percentage = (obtaind/600)* 100;
-        System.out.print("The Percentage of " + name);
-        System.out.println(" is: " + Percentage);
+        System.out.print("Enter The Total Number Of Subjects :");
+        int subjects = sc.nextInt();
+        int sub;
+        double obtained = 0;
+        for(sub = 0; sub < subjects; sub++){
+            int subjectNumber = sub + 1;
+            System.out.print("Subject " + subjectNumber + ":");
+            double marks = sc.nextDouble();
+            obtained = obtained + marks;
+        }
+        double total = subjects * 100;
+        double percentage = (obtained / total) * 100;
+        System.out.println("The Percentage Is :" + percentage);
     }
 }
