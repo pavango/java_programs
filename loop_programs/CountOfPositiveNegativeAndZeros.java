62056d5fec5d4e3b140e5eedb56fc0e28955d277
pavangowda <pavangowda9623@gmail.com>
2020-05-26 19:38:11 +0530

    new programs

---
+++ loop_programs/CountOfPositiveNegativeAndZeros.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class CountOfPositiveNegativeAndZeros {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int positiveNumbers = 0;
+        int negetiveNumbers = 0;
+        int zeros = 0;
+        int i = 0;
+    do {
+            System.out.print("Enter The Number :");
+            int number = sc.nextInt();
+            if (number > 0) {
+                positiveNumbers++;
+            }
+            if (number < 0) {
+                negetiveNumbers++;
+            }
+            if (number == 0) {
+                zeros++;
+            }
+            System.out.print("Give Any number To Continue Or Give 0 To Exit :");
+            i = sc.nextInt();
+        } while(i != 0);
+        System.out.println("In Given Numbers Positive Numbers Are "
+        + positiveNumbers+);
+        System.out.println("In Given Numbers Negative Numbers Are "
+        + negetiveNumbers);
+        System.out.println("In Given Numbers Zeros Are " + zeros );
+    }
+}
---
+++ loop_programs/IncrementingValue.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class IncrementingValue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = 0;
+        int i = 0;
+    do {
+
+            System.out.print("Enter The Number :");
+            int number = sc.nextInt();
+            if (number > 0){
+                x++;
+            }
+
+            System.out.print("Give Any number To Continue Or Give 0 To Exit :");
+            i = sc.nextInt();
+        } while (i != 0);
+        System.out.println("you given inputs " + x + " times");
+    }
+}
