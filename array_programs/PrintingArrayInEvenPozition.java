6b70e292b895b51f81ce46c0d97105cf5765cc2b
pavangowda <pavangowda9623@gmail.com>
2020-06-02 12:20:54 +0530

    Printing Array In Even Pozition

---
+++ array_programs/PrintingArrayInEvenPozition.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class PrintingArrayInEvenPozition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Size Of An Numbers :");
+        int size = sc.nextInt();
+        int[] numbersArray = new int[size];
+        System.out.print("Enter The Elements :");
+        for (int i = 0; i < size; i++) {
+            numbersArray[i] = sc.nextInt();
+        }
+        for (int j = 0 ; j < size; j++) {
+            j = j + 1;
+            System.out.print(numbersArray[j] + " ");
+        }
+        System.out.println();
+    }
+}
