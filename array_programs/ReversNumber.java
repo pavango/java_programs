3865dd0308013dec2cec35387a1a88548a87b3fc
pavangowda <pavangowda9623@gmail.com>
2020-06-02 11:55:06 +0530

    printing revarce of given numbers

---
+++ array_programs/ReversNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class ReversNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Size Of An Numbers :");
+        int size = sc.nextInt();
+        int[] numbersArray = new int[size];
+        System.out.print("Enter The Elements :");
+        for (int i = 0; i < size; i++) {
+            numbersArray[i] = sc.nextInt();
+        }
+        for (int j = size - 1; j >= 0; j--) {
+            System.out.print(numbersArray[j] + " ");
+        }
+        System.out.println();
+    }
+}
