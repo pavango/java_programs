edec64745810ced637140bebf28b3e848967c88e
pavangowda <pavangowda9623@gmail.com>
2020-06-02 11:31:47 +0530

    printing array of given numbers

---
+++ array_programs/PrintingGivenNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class PrintingGivenNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Size Of an Numbers :");
+        int size = sc.nextInt();
+        int[] numbersArray = new int[size];
+        System.out.print("enter the elements :");
+        for (int i = 0; i < size; i++) {
+            numbersArray[i] = sc.nextInt();
+        }
+        for (int j = 0; j < size; j++) {
+            System.out.print(numbersArray[j] + " ");
+        }
+        System.out.println();
+    }
+}
