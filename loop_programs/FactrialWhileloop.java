ab1b815ccf5480c8040225fa7f1cd13bf81d0989
Pavan Gowda H.J <65288338+pavango@users.noreply.github.com>
2020-07-14 17:30:49 +0530

    Update FactrialWhileloop.java

--- loop_programs/FactrialWhileloop.java
+++ loop_programs/FactrialWhileloop.java
@@ -1,30 +1,30 @@
-//importing the Scanner class from util packages
+//importing the Scanner class from util packages,
 import java.util.Scanner;
 
-// defining the class FactrialWhileloop
+// defining the class FactrialWhileloop,
 class FactrialWhileloop {
-    //defining the  main fumnction
+    //defining the  main fumnction,
     public static void main(String[] args) {
-        //initializing the Scanner variable
+        //initializing the Scanner variable,
         Scanner sc = new Scanner(System.in);
-        //printing an instartion to give an number
+        //printing an instartion to give an number,
         System.out.print("Enter The Factorial Number :");
-        // reading an new value
+        // reading an new value,
         int factorialnumber = sc.nextInt();
-        //creating an new veariable and giving an value
+        //creating an new veariable and giving an value,
         int factorial = 1;
-        // crating an new vareiable and giving an value
+        // crating an new vareiable and giving an value,
         int number = 1;
-        // creating an for while loop with creating an variable and storing an value condition and
+        // creating an for while loop with creating an variable and storing an value condition and,
         while (number <= factorialnumber) {
-            // incrementing the variable value
+            // incrementing the variable value,
             number++;
-            //closing the loop
+            //closing the loop,
         }
-        //printing the value of factorial number and factorial with instruction
+        //printing the value of factorial number and factorial with instruction,
         System.out.println("The Factorial Of " + factorialnumber + "! Is "
         + factorial);
-    //closing the main block
+    //closing the main block,
     }
-//closing the class block
+//closing the class block.
 }
