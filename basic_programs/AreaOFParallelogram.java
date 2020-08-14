c7d53a2752949abc7b1b73a3706aa836aea9532e
Pavan Gowda H.J <65288338+pavango@users.noreply.github.com>
2020-08-14 18:06:21 +0530

    Update AreaOFParallelogram.java

--- basic_programs/AreaOFParallelogram.java
+++ basic_programs/AreaOFParallelogram.java
@@ -4,7 +4,7 @@ class AreaOFParallelogram {
     public static void main(StrinG args[]) {
         Scanner sc = new Scanner(System.in);
         int base,hight,area ;
-        System.out.print(":");
+        System.out.print("Enter the value of base:");
         base = sc.nextInt();
         System.out.print("Enter The Value Of Hight:");
         hight = sc.nextInt();
