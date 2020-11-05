3acdc6a60b181fd51327766f02f074fcdedbe23f
pavangowda <pavangowda9623@gmail.com>
2020-11-05 21:50:23 +0530

    new program

--- basic_programs/VolumeOfSphere.java
+++ basic_programs/VolumeOfSphere.java
@@ -4,9 +4,8 @@ import java.lang.Math;
 class VolumeOfSphere {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        double radius;
         System.out.print("Enter The Value Of Radius:");
-        radius = sc.nextDouble();
+        double radius = sc.nextDouble();
         final double pi = 3.14;
         double volume = (4/3) * pi * Math.pow(radius, 3);
         System.out.println("The Volume Of Sphere Is:" + volume);
