fa5d8e7cf56d053d5e608d4b3268e6431ef8c2ee
pavangowda <pavangowda9623@gmail.com>
2020-11-08 21:17:41 +0530

    new program

--- basic_programs/AreaOfPentagon.java
+++ basic_programs/AreaOfPentagon.java
@@ -3,11 +3,10 @@ import java.util.*;
 class AreaOfPentagon {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        double side, length;
         System.out.print("Enter The Value Of Side:");
-        side = sc.nextDouble();
+        double side = sc.nextDouble();
         System.out.print("Enter The Value Of Apothem Length:");
-        length = sc.nextDouble();
+        double length = sc.nextDouble();
         double area = (5/2) * side * length;
         System.out.println("The Volume Of Sphere Is:" + area);
     }
