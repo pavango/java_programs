f8474ecc2e58a3920ff89ce199aaaa0c96a2b1bb
pavangowda <pavangowda9623@gmail.com>
2020-11-16 21:48:41 +0530

    new program

--- src/test/java/com/pavango/numbers/CheckDisariumNumberTests.java
+++ src/test/java/com/pavango/numbers/CheckDisariumNumberTests.java
@@ -13,11 +13,35 @@ class CheckDisariumNumberTests {
         Assertions.assertEquals(true, checked);
     }
     @Test
-    void shouldCheckNumberIsNotDisariumNumber(){
+    void shouldCheckNumberIsNotDisariumNumber() {
         CheckDisariumNumber disariumNumber = new CheckDisariumNumber();
 
         boolean checked = disariumNumber. disariumnumber(120);
 
         Assertions.assertEquals(false, checked);
+    }
+    @Test
+    void shouldCheckTheLengthOfTheGivenNumber() {
+        CheckDisariumNumber lengthOfGivenNumber = new CheckDisariumNumber();
+
+        int checked = lengthOfGivenNumber.lengthOf(784536);
+
+        Assertions.assertEquals(6, checked);
+    }
+    @Test
+    void shouldGivePowerValueToTheGivenNumber() {
+        CheckDisariumNumber powerOf = new CheckDisariumNumber();
+
+        int value = powerOf.powerOf(4, 10);
+
+        Assertions.assertEquals(1048576, value);
+    }
+    @Test
+    void shouldGiveCubeDigitsValueToTheGivenNumber() {
+        CheckDisariumNumber cubeOf = new CheckDisariumNumber();
+
+        int value = cubeOf.getPowerOfDigits(45);
+
+        Assertions.assertEquals(29, value);
     }
 }
