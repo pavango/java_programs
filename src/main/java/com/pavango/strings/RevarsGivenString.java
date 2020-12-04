abbc3142dffa8b22da894e1d36b949dbe28ff4a7
pavangowda <pavangowda9623@gmail.com>
2020-12-04 17:59:15 +0530

    new program

--- src/main/java/com/pavango/strings/RevarsGivenString.java
+++ src/main/java/com/pavango/strings/RevarsGivenString.java
@@ -1,6 +1,6 @@
 package com .pavango.strings;
 
-publiclass RevarsGivenString {
+public class RevarsGivenString {
     public static String revarsTheString (String input) {
         String reversedStr = "";
         for(int i = input.length()-1; i >= 0; i--){
